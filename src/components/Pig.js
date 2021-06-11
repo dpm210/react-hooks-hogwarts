dce962d623ea672906f0d95e3ff5c27ee8c6b3e9
Kaia <kbergmanndean@gmail.com>
2021-06-11 15:21:56 -0400

    toggled greased pigs

--- src/components/Pig.js
+++ src/components/Pig.js
@@ -1,11 +1,31 @@
+import {useState} from "react";
+
+
 function Pig ({ hog }){
+
+    const [togglePig, setTogglePig] = useState(false)
+
+    function handleToggle(){
+        setTogglePig(!togglePig)
+    }
+
+
     const {name, specialty, greased, weight, image} = hog
     const medal = hog["highest medal achieved"]
 
+
     return (
-        <div>
+        <div onClick={handleToggle}>
             <h2>{name}</h2>
+            {togglePig ? <div>
+            <p>{specialty}</p>
+            <p>{weight}</p>
+            <p>{greased ? "is greased" : "is not greased"}</p>
+            <p>{medal}</p>
+            </div>
+            : null}
             <img src= {image}/>
+            
         </div>
 
     )
--- src/components/PigPen.js
+++ src/components/PigPen.js
@@ -1,8 +1,27 @@
 import Pig from './Pig.js'
+import {useState} from "react";
+
 function PigPen({ hogs }){
+
+const [filteredPigs, setFilteredPigs]=useState(hogs)
+const [toggleFilter, setToggleFilter]=useState(false)
+
+function handleFilter(){
+    if(!toggleFilter){
+    const filtered=hogs.filter((hog)=> hog.greased)
+    setFilteredPigs(filtered)
+    setToggleFilter(true)
+} else{
+    setFilteredPigs(hogs)
+    setToggleFilter(false)
+}
+}
+
+
 return (
     <div>
-        {hogs.map(hog => 
+        <button onClick={handleFilter}>{toggleFilter? "Bring me all the pigs!" : "Bring me the greased pigs!"}</button>
+        {filteredPigs.map(hog => 
             <Pig 
             hog={hog}
             key={hog.name}/>
